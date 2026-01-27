91327b420a3065629d9ab39b307a112e52414ce3
cwlebgarcia <calebf3g4@gmail.com>
2026-01-27 16:41:53 -0700

    Started lab report

---
+++ Lab-1/Lab 1 Analysis.py
@@ -0,0 +1,73 @@
+import numpy as np
+from numpy.polynomial import Polynomial
+import seaborn as sns
+import scipy as sci
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from scipy.constants import mu_0
+
+data_path: str = 'Lab-1/raw_csv_data'
+graphs_path: str = 'Lab-1/Graphs'
+
+resistance: float = 100e3 # 100k resistor
+diameter: float = 33.5e-3
+area: float = np.pi * diameter ** 2 / 4
+
+for file in os.listdir(data_path):
+    if file.endswith('.csv'):  
+        file_path = os.path.join(data_path, file) 
+        df = pd.read_csv(file_path)  
+    print(file)
+
+    x = df['second']
+    y = df['Volt']
+
+    finite_mask = np.isfinite(y) # Mask to remove NaNs and infs
+
+    x = x[finite_mask]
+    y = y[finite_mask]
+
+    time_mask = np.abs(x) < 0.045 # Truncate large time values
+
+    x = x[time_mask]
+    y = y[time_mask]
+    
+    # Polynomial fit for denoising
+    coefficients = Polynomial.fit(x=x, y=y, deg=200)
+    fit_polynomial = coefficients.convert()
+    voltage = fit_polynomial(x)
+    
+    flux = -1 * np.cumsum(voltage)
+
+    # Plot voltage
+    ax = sns.scatterplot(x=x, y=voltage)
+    plt.title(f'{file} Voltage')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Voltage (V)')
+    plt.savefig(f'{graphs_path + '/' + file} voltage.png')
+    plt.close()
+
+    # Plot current
+    ax = sns.scatterplot(x=x, y=voltage / resistance)
+    plt.title(f'{file} Current')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Current (A)')
+    plt.savefig(f'{graphs_path + '/' + file} current.png')
+    plt.close()
+
+    # Plot flux
+    ax = sns.scatterplot(x=x, y=flux)
+    plt.title(f'{file} Flux')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Magnetic Flux (Wb)')
+    plt.savefig(f'{graphs_path + '/' + file} Flux.png')
+    plt.close()
+
+    # Plot magnetic field
+    ax = sns.scatterplot(x=x, y=flux * area / mu_0)
+    plt.title(f'{file} Magnetic Field')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Magnetic Field (T)')
+    plt.savefig(f'{graphs_path + '/' + file} Magnetic Field.png')
+    plt.close()
