0798fdeadea402d93e5e78d57e43038275586239
cwlebgarcia <calebf3g4@gmail.com>
2026-02-21 22:31:17 -0700

    Comment

--- UPW_Calculator.py
+++ UPW_Calculator.py
@@ -21,7 +21,8 @@ calc_general_theta_eta = lambda conductivity, angular_frequency, relative_permit
         )
 
 if __name__ == "__main__":
-    f = 10e6 # GHz
+    
+    f = 10e6 # Hz
     angular_frequency = 2 * np.pi * f
     relative_permittivity = 4
     relative_permeability = 1
